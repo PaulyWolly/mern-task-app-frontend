f542e4ade62b1283ec806f93fd5821b077103e6e
Paul Welby <pwelby@gmail.com>
2023-07-04 09:31:19 -0700

    Initial commit for frontend MERN-TASK-APP

---
+++ src/components/TaskForm.js
@@ -0,0 +1,19 @@
+
+const TaskForm = ({createTask, name, handleInputChange, isEditing, updateTask}) => {
+  return (
+    <div>
+      <form className="task-form" onSubmit={isEditing ? updateTask : createTask}>
+        <input
+          type="text"
+          placeholder="Add a task"
+          name="name"
+          value={name}
+          onChange={handleInputChange}
+        />
+        <button type="submit">{isEditing ? "Edit" : "Add"}</button>
+      </form>
+    </div>
+  )
+}
+
+export default TaskForm
